0506890f16c73b9451b7ad658566d4e8b2f3f7b5
Adam Casey <acasey23@bloomberg.net>
2023-11-27 11:29:22 +0000

    Fix include guard of rmqa_connectionstring
    This include guard is wrong, which can cause issues when trying to use the two headers rmqa_connectionstring.h and rmqa_connectionmonitor.h in the same TU.
    
    Signed-off-by: Adam Casey <acasey23@bloomberg.net>

--- src/rmq/rmqa/rmqa_connectionstring.h
+++ src/rmq/rmqa/rmqa_connectionstring.h
@@ -13,8 +13,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#ifndef INCLUDED_RMQA_CONNECTIONMONITOR
-#define INCLUDED_RMQA_CONNECTIONMONITOR
+#ifndef INCLUDED_RMQA_CONNECTIONSTRING
+#define INCLUDED_RMQA_CONNECTIONSTRING
 
 #include <rmqt_vhostinfo.h>
 
